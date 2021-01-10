391cb6618e3c511ff46f1edfdaa661a2d9e1a4b6
MerryDev <svgr1808@outlook.de>
2021-01-10 20:31:16 +0100

    Added more colors

--- src/main/java/de/digitaldevs/bot/utils/Color.java
+++ src/main/java/de/digitaldevs/bot/utils/Color.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 public enum Color {
 
   RED("[color=red]", "[/color]"),
-  GREEN("[color=green]", "[/color]")
+  GREEN("[color=green]", "[/color]"),
+  ORANGE("[color=orange]", "[/color]"),
+  DEEP_SKY_BLUE("[color=deepskyblue]", "[/color]")
   ;
 
   @Getter private final String NAME;
