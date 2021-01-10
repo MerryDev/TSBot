101437d29b41988381333b7b6bb1848832332484
MerryDev <svgr1808@outlook.de>
2021-01-10 22:59:18 +0100

    Reworked code for more clarity

--- src/main/java/de/digitaldevs/bot/Var.java
+++ src/main/java/de/digitaldevs/bot/Var.java
@@ -1,9 +1,13 @@
 package de.digitaldevs.bot;
 
+import java.util.HashMap;
+
 public class Var {
 
   public static final String APPLICATION_PATH = System.getProperty("user.dir");
 
   public static String host, port, serverID, queryName, password, teamrankID, nickname;
+
+  public static HashMap<Integer, Boolean> users = new HashMap<>();
 
 }
